235c4ce4eb56b285f0962bd7ef2aaa51a117be8c
fkoeh <fkoeh@BUNKERHQ>
2024-09-12 17:51:25 +0200

    Add project files.

---
+++ EF Core Tut 1/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace EF_Core_Tut_1.Models
+{
+	public class Order
+	{
+		public int Id { get; set; }
+		public DateTime OrderPlaced { get; set; }
+		public DateTime? OrderFulfilled { get; set; }
+		public int CustomerId { get; set; }
+		public Customer Customer { get; set; } = null!;
+		public ICollection<OrderDetail> OrderDetails { get; set; } = null!;
+
+	}
+}
---
+++ EF Core Tut 1/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EF_Core_Tut_1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal Price { get; set; }
+    }
+} 
